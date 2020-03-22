3e2c0e0e7c55215f5ef925bf0b8db6b23989f859
kealanheena <kealanheena@gmail.com>
2020-03-22 20:18:01 +0000

    Chore: add snapshot test for App

--- src/components/Glucometer/Glucometer.jsx
+++ src/components/Glucometer/Glucometer.jsx
@@ -3,7 +3,21 @@ import React, { Component } from 'react';
 class Glucometer extends Component {
   state = {
     // value to be displayed in <Display />
-    displayValue: '',
+    displayValue: 'Welcome to the online carb counter',
+    // value to be displayed in the <Keys />
+    buttons: ['Get Bolus'],
+    // ratio used to calculate your bolus(amount of insulin needed)
+    ratio: '',
+    // amount of carbs you are eating in grams
+    carbs: ''
+  }
+
+  calculateBolus = () => {
+    console.log('calculateBolus')
+  }
+
+  updateDisplay = () => {
+    console.log('updateDisplay')
   }
 
   render = () => {
